72ecf185ee444f96dae63fe333f6cb06573f1212
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2018-05-03 09:37:46 -0300

    implementa parte do servidor usando if (estado) com switch(comando)

--- server.ts
+++ server.ts
@@ -6,75 +6,127 @@ class PTAServer extends Server {
 
     // define a porta e a lista de arquivos
     private files: Array<string> = ['file1.txt', 'file2.txt', 'file3.txt'];
-    private seq_num: number = 0; // número da sequência começa em 0
-    connection: Socket = new Socket();
+    // private seq_num: number = 0; // número da sequência começa em 0
+    private ready: boolean = false;
     
-    constructor() { super(); }
+    constructor(listener?: ((socket: Socket) => void)) { super(listener); }
 
-    getSeq_num(): number {
-        return this.seq_num;
+    /**
+     * alterna o estado da FSM
+     */
+    toggleState(): void {
+        this.ready = this.ready ? !this.ready : this.ready;
+    }
+
+    // getters
+    isReady(): boolean {
+        return this.ready;
     }
 
     getFiles(): Array<string> {
         return this.files;
     }
 
-    respond(reply: string): void {
-        this.connection.write(++this.seq_num + ' ' + reply);
-    }
-
 }
 
-// cria o servidor e retorna a conexão com o client
-/*const server = createServer((connection) => {
-    // executa a função quando recebe algum dado
-    connection.on('data', (message) => {
-        let request = message.toString().split(' ');
-        var seq_num = request[0];
-        var command = request[1];
+// declara a função respond
+var respond: ((reply: string, args?: string) => void);
 
-        // função criada para facilitar a resposta ao client
-        function response(res: string): void {
-            connection.write(seq_num + ' ' + res);
+const pta = new PTAServer((connection) => {
+    connection.on('data', (data: Buffer) => {
+        var message = data.toString().split(' ');
+        var seq_num = message[0];
+        var command = message[1];
+
+        // função para responder ao cliente
+        respond = (reply: string, args?: string): void => {
+            if (args) {
+                connection.write(seq_num + ' ' + reply + ' ' + args);
+            } else {
+                connection.write(seq_num + ' ' + reply);
+            }
         }
 
-        switch (command) {
-            case 'CUMP':
-                let name = request[2];
-                if (name === 'client') {
-                    response('OK');
+        if (pta.isReady()) {
+            switch (command) {
+                case 'TERM':
                     break;
-                }
-                response('NOK');
-                connection.end();
-                break;
-            case 'LIST':
-                response('NOK');
-                break;
-            case 'PEGA':
-                let fileName = request[1];
-                var i: number = 0;
-                files.forEach((value, index) => {
-                    if (value === fileName) {
-                        i = index;
+                case 'PEGA':
+                    break;
+                case 'LIST':
+                    break;
+                default:
+                    respond('NOK');
+                    break;
+            }
+        } else {
+            switch (command) {
+                case 'CUMP':
+                    let args = message[2];
+                    if (args === 'client') {
+                        respond('OK');
+                        pta.toggleState();
+                    } else {
+                        respond('NOK');
                     }
-                });
-                if (i) response('SEND' + files[i]);              
-                break;
-            case 'TERM':
-                response('OK');
-                connection.end();
-                break;
-            default:
-                response('NOK');
-                break;
+                    break;
+            
+                default:
+                    break;
+            }
         }
-    });
 
+        // switch (command) {
+        //     case 'CUMP':
+        //         if (pta.isReady()) {
+        //             respond('NOK');
+        //         } else {
+                    
+        //         }
+        //         break;
+        //     case 'LIST':
+        //         if (pta.isReady()) {
+        //             respond('ARQS', pta.getFiles().toString());
+        //         } else {
+        //             respond('NOK');
+        //             connection.end();
+        //         }
+        //         break;
+        //     case 'PEGA':
+        //         let args = message[2];
+        //         let files = pta.getFiles();
+        //         var i: number = 0;
+        //         files.forEach((value, index) => {
+        //             if (args == value) {
+        //                 i = index;
+        //             }
+        //         });
+        //         respond('ARQ', files[i]);
+        //         break;
+        //     case 'TERM':
+        //         if (pta.isReady()) {
+        //             respond('OK');
+        //             connection.end();
+        //             pta.toggleState();
+        //         } else {
+        //             respond('NOK');
+        //             connection.end();
+        //         }
+        //         break;
+        //     default:
+        //         if (pta.isReady()) {
+        //             respond('NOK');
+        //         } else {
+        //             respond('NOK');
+        //             connection.end();
+        //         }
+        //         break;
+        // }
+    });
+    connection.on('close', (had_error) => {
+        if (had_error) console.log('Error 500');
+    });
 });
 
-// começa a escutar requisições
-server.listen(port);*/
-
-var pta = new PTAServer();
+// começa a escutar as requisições
 pta.listen(port);
