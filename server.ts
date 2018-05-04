64f211a7e9c77de262c9011666326e524406c7f3
Matheus Campos da Silva <silva.campos.matheus@gmail.com>
2018-05-04 12:09:34 -0300

    Adiciona a data e hora na mensagem de conexão / desconexão

--- server.ts
+++ server.ts
@@ -4,9 +4,8 @@ const port: number = 11550;
 
 class PTAServer extends Server {
 
-    // define a porta e a lista de arquivos
+    // define a lista de arquivos e o estado do servidor
     private files: Array<string> = ['file1.txt', 'file2.txt', 'file3.txt'];
-    // private seq_num: number = 0; // número da sequência começa em 0
     private ready: boolean = false;
     
     constructor(listener?: ((socket: Socket) => void)) { super(listener); }
@@ -23,12 +22,22 @@ class PTAServer extends Server {
     }
 
     // getters
+    getFiles(): Array<string> {
+        return this.files;
+    }
+
     isReady(): boolean {
         return this.ready;
     }
 
-    getFiles(): Array<string> {
-        return this.files;
+    /**
+     * retorna a data e hora atual
+     */
+    now(): string {
+        let date = new Date();
+        let dateArray: any = date.toLocaleDateString().split('-');
+        dateArray = dateArray.reverse().join('/');
+        return dateArray + ' ' + date.toLocaleTimeString();
     }
 
 }
@@ -37,18 +46,12 @@ class PTAServer extends Server {
 var respond: ((reply: string, args?: string) => void);
 
 const pta = new PTAServer((connection: Socket) => {
-    console.log('client connected [' + connection.remoteAddress + ':' + connection.remotePort + ']');
+    console.log(pta.now() + ' - Client connected [' + connection.remoteAddress + ':' + connection.remotePort + ']');
 
     connection.on('data', (data: Buffer) => {
         var message = data.toString().split(' ');
         var seq_num = message[0];
         var command = message[1];
-
-        // if (seq_num != '0' || !command) {
-        //     connection.write('500\n');
-        //     connection.end();
-        //     return;
-        // }
 
         // função para responder ao cliente
         respond = (reply: string, args?: string): void => {
@@ -121,12 +124,12 @@ const pta = new PTAServer((connection: Socket) => {
         if (pta.isReady()) {
             pta.toggleState();
         }
-        console.log('client disconnected [' + connection.remoteAddress + ':' + connection.remotePort + ']');
+        console.log(pta.now() + ' - Client disconnected [' + connection.remoteAddress + ':' + connection.remotePort + ']');
     });
 
 });
 
 // começa a escutar as requisições
 pta.listen(port);
-console.log('processo ' + process.pid + ' rodando na plataforma ' + process.platform);
-console.log('Escutando na porta 11550...\n');
+console.log('Process ' + process.pid + ' running on ' + process.platform);
+console.log('Listening on port 11550...\n');
