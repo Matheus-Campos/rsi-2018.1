a7095e23eb1b0f66db54a7e40ba26d39acc1e141
Matheus-Campos <silva.campos.matheus@gmail.com>
2018-07-27 15:27:52 -0300

    Cria o cliente ipv6 e comenta o código desenvolvido

--- ipv6/client.ts
+++ ipv6/client.ts
@@ -1,6 +1,22 @@
-import { createConnection } from 'net';
+// importa os métodos necessários
+import { connect } from 'net';
 
+// define as variáveis com base nos argumentos
+const port: number = 5000;
 const host: string = process.argv[2];
 const msg: string = process.argv[3];
 
+// cria a conexão com o servidor
+const connection = connect({
+    host: host,
+    port: port
+});
 
+// configura para imprimir os dados recebidos do servidor
+connection.on('data', (data: Buffer) => {
+    console.log(data.toString());
+    process.exit();
+});
+
+// envia a mensagem obtida pelos argumentos
+connection.write(msg);
--- ipv6/server.ts
+++ ipv6/server.ts
@@ -1,16 +1,21 @@
+// importa as classes necessárias
 import { Server, Socket } from 'net';
 
+// define a porta do servidor
 const port: number = 5000;
 
+// cria o handler para o recebimento de dados por parte do cliente
 const handler = (connection: Socket) => {
     connection.on('data', (data: Buffer) => {
         connection.write(data.toString().toUpperCase());
     });
 };
 
+// instancia 2 servidores com o mesmo handler (mesma lógica)
 var server = new Server(handler);
 var server6 = new Server(handler);
 
+// cada servidor escuta em 1 interface diferente
 server.listen(port, '127.0.0.1');
 server6.listen(port, '::1');
 
